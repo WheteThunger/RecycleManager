ae05eb8552c1de5b74f9e877a01d72dac4cf0fab
WhiteThunder <whetethunger@gmail.com>
2024-01-10 02:29:51 -0800

    Increment patch version

--- RecycleManager.cs
+++ RecycleManager.cs
@@ -19,7 +19,7 @@ using Oxide.Core.Plugins;
 
 namespace Oxide.Plugins
 {
-    [Info("Recycle Manager", "WhiteThunder", "2.0.3")]
+    [Info("Recycle Manager", "WhiteThunder", "2.0.4")]
     [Description("Allows customizing recycler speed, input, and output")]
     internal class RecycleManager : CovalencePlugin
     {
